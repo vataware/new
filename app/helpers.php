2f228ad8b3de132b3cce877b6513038b24b218b1
Roy de Vos Burchart <roy.devosburchart@comayers.com>
2014-04-16 14:58:37 +0200

    Helper functions and css

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,18 @@ View::share('build', $build);
 function flag($code) {
 	return asset('assets/images/flags/' . $code . '.png');
 }
+
+function piechartData($data) {
+	$colours = ['#48CA3B', '#00BCE1', '#4D3A7D', '#AD1D28', '#DEBB27', '#DF6E1E'];
+	shuffle($colours);
+	$colours[] = '#111111';
+	$colourCount = count($colours)+1;
+	
+	$results = array();
+
+	foreach($data as $key => $entry) {
+		$results[] = '{ label: "' . $entry[0] . '",  data: ' . $entry[1] . ', color: "' . (isset($entry[2]) ? $entry[2] : $colours[$key%$colourCount]) . '"}';
+	}
+
+	return implode(",\n", $results);
+}
--- public/assets/javascript/vataware.js
+++ public/assets/javascript/vataware.js
@@ -12,3 +12,39 @@ $(document).ready(function() {
 		container: 'body'
 	});
 });
+
+function createPieChart(element, data) {
+	$.plot(element, data, {
+		series: {
+			pie: {
+				show: true,
+				radius: 110,
+				highlight: {
+					opacity: 0.25
+				},
+				stroke: {
+					color: '#fff',
+					width: 2
+				},
+				// startAngle: 0,
+				label: {
+					show: true,
+					radius: 3/4,
+					formatter: function(label, series) {
+					 	return '<div class="chart-label">' + label + '</div>';
+					},
+					background: { 
+						opacity: 0.6,
+						color: '#000'
+					}
+				}
+			}
+		},
+		legend: {
+			show: false,
+		},
+		grid: {
+			hoverable: true,
+		},
+	});
+}
